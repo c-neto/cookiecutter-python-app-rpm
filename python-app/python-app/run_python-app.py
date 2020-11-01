bc73d418de792047416d247ecf2e1300f17f895c
augustoliks <carlos.santos110@fatec.sp.gov.br>
2020-11-01 16:51:57 -0300

    remove samples files

--- python-app/python-app/run_python-app.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-print('hello augustoliks !!!')
-print('test external dependencies... ')
-res = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(f'get fake rest-api:  {res.json()}')
